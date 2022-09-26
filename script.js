233580d11f84aa3bdc339448cdae5bf367892f77
Filip Michálek <vsechnouzjezabrany@gmail.com>
2022-09-26 19:15:30 +0200

    change

--- script.js
+++ script.js
@@ -7,10 +7,12 @@ function preventScroll(e){
 
 function disable(){
   document.querySelector('.scrollable').addEventListener('wheel', preventScroll);
+  document.querySelector('.scrollable').addEventListener('touchmove', preventScroll);
 }
 
 function enable(){
     document.querySelector('.scrollable').removeEventListener('wheel', preventScroll);
+    document.querySelector('.scrollable').removeEventListener('touchmove', preventScroll);
 }
 
 document.querySelector('#prevent').addEventListener('click', disable);
